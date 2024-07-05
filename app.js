030e6fe99cfc019bf0cbf7674d58626659a1ee8f
EZIO AUDITORE <ezioauditoredefinerz@gmail.com>
2024-07-06 01:23:36 +0530

    css nd html commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.json());
 
@@ -12,6 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('src'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/inde.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
+
+
+
+
 
